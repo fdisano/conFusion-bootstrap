aee029eef7cdc544a8ad6df2b5d55542017289e8
fdisano <feliciadisano@gmail.com>
2022-09-07 12:35:43 -0400

    npm scripts part 1

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $("#mycarousel").carousel({ interval: 2000 });
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("span").hasClass("fa-pause") ){
+            $("#mycarousel").carousel("pause");
+            $("#carouselButton").children("span").removeClass("fa-pause");
+            $("#carouselButton").children("span").addClass("fa-play");
+        }
+        else if ($("#carouselButton").children("span").hasClass("fa-play")){
+            $("#mycarousel").carousel("cycle");
+            $("#carouselButton").children("span").removeClass("fa-play");
+            $("#carouselButton").children("span").addClass("fa-pause");
+        }
+    });
+
+    // Login Modal
+    $("#loginButton").click(function(){
+      $("#loginModal").modal("show");
+    });
+    $("#hideLoginModal").click(function(){
+      $("#loginModal").modal("hide");
+    });
+    $("#cancelLogin").click(function(){
+      $("#loginModal").modal("hide");
+    });
+
+    // Reserve Modal
+    $("#reserveButton").click(function(){
+      $("#reserveModal").modal("show");
+    });
+    $("#hideReserveModal").click(function(){
+      $("#reserveModal").modal("hide");
+    });
+    $("#cancelReserve").click(function(){
+      $("#reserveModal").modal("hide");
+    });
+});
